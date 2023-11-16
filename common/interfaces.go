b459a0d15e5b6d97e8668df3ea3287de80e21518
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-11-16 09:19:24 +0100

    chore(deps): update dependencies

--- common/interfaces.go
+++ common/interfaces.go
@@ -19,12 +19,12 @@ type Interceptor interface {
 type Configurer interface {
 	// UnmarshalKey takes a single key and unmarshal it into a Struct.
 	UnmarshalKey(name string, out any) error
-	// Has checks if config section exists.
+	// Has checks if a config section exists.
 	Has(name string) bool
 }
 
 type Pool interface {
-	// Workers returns worker list associated with the pool.
+	// Workers return a worker list associated with the pool.
 	Workers() (workers []*worker.Process)
 	// Exec payload
 	Exec(ctx context.Context, p *payload.Payload, stopCh chan struct{}) (chan *staticPool.PExec, error)
@@ -34,8 +34,10 @@ type Pool interface {
 	AddWorker() error
 	// Reset kill all workers inside the watcher and replaces with new
 	Reset(ctx context.Context) error
-	// Destroy all underlying stack (but let them complete the task).
+	// Destroy the underlying stack (but let them complete the task).
 	Destroy(ctx context.Context)
+	// QueueSize shows the number of requests in the queue
+	QueueSize() uint64
 }
 
 type Logger interface {
--- tests/go.mod
+++ tests/go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/roadrunner-server/otel/v4 v4.3.6
 	github.com/roadrunner-server/resetter/v4 v4.0.15
 	github.com/roadrunner-server/rpc/v4 v4.2.9
-	github.com/roadrunner-server/server/v4 v4.5.0
+	github.com/roadrunner-server/server/v4 v4.5.1
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.26.0
 	google.golang.org/grpc v1.59.0
@@ -92,7 +92,7 @@ require (
 	go.opentelemetry.io/otel/sdk v1.20.0 // indirect
 	go.opentelemetry.io/otel/trace v1.20.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
-	go.temporal.io/api v1.25.0 // indirect
+	go.temporal.io/api v1.26.0 // indirect
 	go.temporal.io/sdk v1.25.1 // indirect
 	go.temporal.io/sdk/contrib/opentelemetry v0.3.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
