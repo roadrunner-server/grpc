614c8fbaba90ff7b38a069d628f67afc8dd61855
Nicolai Cornelis <nickdnk@hotmail.com>
2024-10-24 18:10:07 +0200

    Add json schema Remove redundant CA cert check

--- config.go
+++ config.go
@@ -178,7 +178,7 @@ func (c *Config) InitDefaults() error { //nolint:gocyclo,gocognit
 
 func (c *Config) EnableTLS() bool {
 	if c.TLS != nil {
-		return (c.TLS.RootCA != "" && c.TLS.Key != "" && c.TLS.Cert != "") || (c.TLS.Key != "" && c.TLS.Cert != "")
+		return c.TLS.Key != "" && c.TLS.Cert != ""
 	}
 	return false
 }
