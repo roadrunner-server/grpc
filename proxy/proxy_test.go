f7f48590bc3092b90f2f49dbcd2b135fd58fc1ee
Valery Piashchynski <piashchynski.valery@gmail.com>
2025-02-12 14:34:53 +0100

    fix: do not try to call Error() on nil err
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -15,6 +15,36 @@ func TestWrapError(t *testing.T) {
 	require.Equal(t, "rpc error: code = PermissionDenied desc = Unauthorized access `index`", newErr.Error())
 }
 
+func err() error {
+	return err2()
+}
+
+func err2() error {
+	return errors.E("some_operation", err3())
+}
+
+func err3() error {
+	return errors.E("some_operation", errors.NoFreeWorkers)
+}
+
+func err4() error {
+	return errors.E(err5())
+}
+
+func err5() error {
+	return nil
+}
+
+func TestWrapDifferentError(t *testing.T) {
+	newErr := wrapError(err())
+	require.Contains(t, newErr.Error(), "NoFreeWorkers:\n\tsome_operation")
+}
+
+func TestWrapNilError(t *testing.T) {
+	newErr := wrapError(err4())
+	require.Contains(t, newErr.Error(), "rpc error: code = Internal desc = unknown type <nil>, value <nil> in error call")
+}
+
 func TestRRErrorPackage(t *testing.T) {
 	msg := "7|:|Unauthorized access `index`|:|\n(type.googleapis.com/google.rpc.ErrorInfo\u0012_\n\u0010PermissionDenied\u0012#app.ServiceName\u001a&\n\u0007message\u0012\u001bUnauthorized access `index`"
 	const op1 = errors.Op("foo_op")
